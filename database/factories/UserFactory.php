6a322ed5db45d06fec2c1d8e972ac68713da8892
Cory Bateman <coryb342@gmail.com>
2025-10-09 22:31:33 -0700

    Add hasFactory attribute to Organization class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'org_id' => OrganizationFactory::create()->id,
+            'org_id' => Organization::factory(1)->create()->first()->id,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
