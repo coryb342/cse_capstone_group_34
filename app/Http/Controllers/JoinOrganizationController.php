71477872e416b6e75e30d6f12014ac4987bdbd29
Cory Bateman <coryb342@gmail.com>
2025-10-28 17:43:17 -0700

    Create Join Organization page and handle form submission

---
+++ app/Http/Controllers/JoinOrganizationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrgAccessCode;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+
+
+class JoinOrganizationController extends Controller
+{
+    public function index() {
+        return Inertia::render('auth/JoinOrganization', []);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'org_access_code' => ['required', 'integer'],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $active_access_code = OrgAccessCode::query()->where('access_code', '=', $request->get('org_access_code'))->where('is_active', '=', true)->first();
+
+        if ($active_access_code && !$active_access_code->isExpired()) {
+
+            $active_access_code->update(['is_active' => false]);
+
+            $user = User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'password' => Hash::make($request->get('password')),
+                'org_id' => $active_access_code->org_id
+            ]);
+
+            event(new Registered($user));
+
+            Auth::login($user);
+
+            $request->session()->regenerate();
+
+            return to_route('dashboard');
+
+        } else {
+            return Redirect::back()->withErrors(['org_access_code' => 'Invalid access code.']);
+        }
+    }
+}
---
+++ resources/js/pages/auth/JoinOrganization.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+import InputError from '@/components/InputError.vue';
+import TextLink from '@/components/TextLink.vue';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import AuthBase from '@/layouts/AuthLayout.vue';
+import { login } from '@/routes';
+import { Form, Head } from '@inertiajs/vue3';
+import { LoaderCircle } from 'lucide-vue-next';
+import JoinOrganizationController from '@/actions/App/Http/Controllers/JoinOrganizationController';
+import {usePage} from '@inertiajs/vue3';
+
+const page = usePage()
+const errors = page.props.errors
+const flash = page.props.flash
+</script>
+
+<template>
+    <AuthBase
+        title="Join your Organization"
+        description="Enter Organization and User Details"
+    >
+        <Head title="Join Organization" />
+
+        <Form
+            v-bind="JoinOrganizationController.store.form()"
+            :reset-on-success="['password', 'password_confirmation']"
+            v-slot="{ errors, processing }"
+            class="flex flex-col gap-6"
+        >
+            <div class="grid gap-6">
+
+                <h2>Organization Details</h2>
+
+                <div class="grid gap-2">
+                    <Label for="num_seats">Organization Access Code</Label>
+                    <Input
+                        id="org_access_code"
+                        type="number"
+                        required
+                        autofocus
+                        :tabindex="1"
+                        autocomplete="org_access_code"
+                        name="org_access_code"
+                    />
+                    <InputError :message="errors.org_access_code" />
+                </div>
+            </div>
+            <div class="grid gap-6">
+
+                <h2>User Details</h2>
+
+                <div class="grid gap-2">
+                    <Label for="name">Name</Label>
+                    <Input
+                        id="name"
+                        type="text"
+                        required
+                        autofocus
+                        :tabindex="1"
+                        autocomplete="name"
+                        name="name"
+                        placeholder="Full name"
+                    />
+                    <InputError :message="errors.name" />
+                </div>
+
+                <div class="grid gap-2">
+                    <Label for="email">Email address</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        required
+                        :tabindex="2"
+                        autocomplete="email"
+                        name="email"
+                        placeholder="email@example.com"
+                    />
+                    <InputError :message="errors.email" />
+                </div>
+
+                <div class="grid gap-2">
+                    <Label for="password">Password</Label>
+                    <Input
+                        id="password"
+                        type="password"
+                        required
+                        :tabindex="3"
+                        autocomplete="new-password"
+                        name="password"
+                        placeholder="Password"
+                    />
+                    <InputError :message="errors.password" />
+                </div>
+
+                <div class="grid gap-2">
+                    <Label for="password_confirmation">Confirm password</Label>
+                    <Input
+                        id="password_confirmation"
+                        type="password"
+                        required
+                        :tabindex="4"
+                        autocomplete="new-password"
+                        name="password_confirmation"
+                        placeholder="Confirm password"
+                    />
+                    <InputError :message="errors.password_confirmation" />
+                </div>
+
+                <Button
+                    type="submit"
+                    class="mt-2 w-full"
+                    tabindex="5"
+                    :disabled="processing"
+                    data-test="register-user-button"
+                >
+                    <LoaderCircle
+                        v-if="processing"
+                        class="h-4 w-4 animate-spin"
+                    />
+                    Join Organization
+                </Button>
+            </div>
+
+            <div class="text-center text-sm text-muted-foreground">
+                Already have an account?
+                <TextLink
+                    :href="login()"
+                    class="underline underline-offset-4"
+                    :tabindex="6"
+                >Log in</TextLink
+                >
+            </div>
+        </Form>
+    </AuthBase>
+</template>
