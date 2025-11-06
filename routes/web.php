055c0c05f1cf5e0b65d095465adfaf915e98e6fa
Cory Bateman <coryb342@gmail.com>
2025-11-06 10:04:44 -0800

    Add necessary routes for user management page.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserManagementController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,13 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/manage-users', [UserManagementController::class, 'index'])->middleware(['auth', 'verified'])->name('user-management');
+Route::post('/manage-users/toggle-admin', [UserManagementController::class, 'toggleAdmin'])->middleware(['auth', 'verified'])->name('toggle-admin');
+Route::post('/manage-users/toggle-status', [UserManagementController::class, 'toggleStatus'])->middleware(['auth', 'verified'])->name('toggle-status');
+Route::post('/manage-users/delete-user', [UserManagementController::class, 'deleteUser'])->middleware(['auth', 'verified'])->name('delete-user');
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
