ea0a88a9d5208eeb4fd522cc6553a8876c339725
Cory Bateman <coryb342@gmail.com>
2025-11-26 19:41:07 -0800

    Add the script to run .joblib files

---
+++ run_prediction.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import joblib
+import pandas as pd
+
+def main():
+    MODEL_PATH = os.getenv("MODEL_PATH")
+    model_package = joblib.load(MODEL_PATH)
+    model = model_package["model"]
+    features = model_package["features"]
+
+    input_features = sys.argv[1:]
+    if len(input_features) != len(features):
+        print(f"Error: Expected {len(features)} features, got {len(input_features)}", file=sys.stderr)
+        sys.exit(1)
+
+    input_floats = list(map(float, input_features))
+
+    X = pd.DataFrame([input_floats], columns=features)
+
+    prediction = model.predict(X)[0]
+
+    print(prediction)
+
+if __name__ == "__main__":
+    main()
