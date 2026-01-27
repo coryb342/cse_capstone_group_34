6ad89653b00ec6d07b1ff67dd5c115fff28c09fe
Cory Bateman <coryb342@gmail.com>
2026-01-26 16:12:54 -0800

    Create seeder for default roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,10 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RunSeeder::class,
         ]);
+
+        $this->call([
+            RoleSeeder::class,
+        ]);
 //        User::factory()->create([
 //            'name' => 'Test User',
 //            'email' => 'test@example.com',
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ["Super", "Admin", "User"];
+        foreach ($roles as $role) {
+            Role::firstOrCreate(['name' => $role]);
+        }
+    }
+}
