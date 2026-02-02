787cf322a9e179a549b7c72177c0a683515f60b3
Tim Hand <thand556@gmail.com>
2026-02-02 10:40:50 -0800

    Added Our names and advisors

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,33 @@
 <!DOCTYPE html>
-<h1>About Page</h1>
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+</head>
+<body>
+<h1>Flow and Sensor Forecast</h1>
+<p>
+    Department of Computer Science & Engineering<br>
+    College of Engineering, University of Nevada, Reno<br>
+    CS 426 Senior Projects in Computer Science<br>
+</p>
+<h2>Group 34</h2>
+<p> Cory Bateman<br>
+    Timothy Hand<br>
+    Bohdi Norvell
+</p>
+
+<h2>Instructors & TAs</h2>
+<p>
+    Dave Feil-Seifer<br>
+    Vinh Le<br>
+    Stosh Peterson<br>
+    Richie White
+</p>
+
+<h2>External Advisors</h2>
+<p>
+    Austin Martin (Process Control Engineer, TMWRF)
+</p>
+
 <button onclick="window.history.back()">Go Back</button>
+</body>
