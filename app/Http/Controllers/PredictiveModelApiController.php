3632ae743397d1dd7c1ee0877861962c866fa5f0
Cory Bateman <coryb342@gmail.com>
2026-02-19 22:55:27 -0800

    Add rate limiting to describe function

--- app/Http/Controllers/PredictiveModelApiController.php
+++ app/Http/Controllers/PredictiveModelApiController.php
@@ -2,21 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PredictiveModel;
 use App\Models\PredictiveModelAccessToken;
 use App\Services\PredictiveModelApiService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class PredictiveModelApiController extends Controller
 {
+    private const API_RATE_LIMIT_PER_MIN = 2;
+    private const API_RATE_LIMIT_KEY_PREFIX = 'api-hit:';
     public function __construct(
         protected PredictiveModelApiService $api_service
     )
     {
     }
 
-    public function describe(Request $request): JsonResponse
+    public function describeModel(Request $request): JsonResponse
     {
+        $rate_limit_key = self::API_RATE_LIMIT_KEY_PREFIX . $request->ip();
+
+        if (RateLimiter::tooManyAttempts($rate_limit_key, $perMinute = self::API_RATE_LIMIT_PER_MIN)) {
+            return response()->json(['error' => 'Too many attempts.'], 429);
+        }
 
         $validated_token = $this->tokenResolver($request);
 
@@ -25,8 +34,8 @@ class PredictiveModelApiController extends Controller
         }
 
         $predictive_model_id = $validated_token->model_id;
-
         $response = $this->api_service->describe($predictive_model_id);
+        RateLimiter::hit($rate_limit_key, 60);
 
         if (!$response) {
             return response()->json(['error' => 'Not Found'], 404);
