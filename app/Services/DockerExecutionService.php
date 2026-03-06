b716a3f18012537f07f13b4f2f94f140a4099e1a
Cory Bateman <coryb342@gmail.com>
2026-03-05 23:43:23 -0800

    Create backend support for gathering model information

--- app/Services/DockerExecutionService.php
+++ app/Services/DockerExecutionService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use App\Models\PredictiveModel;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
@@ -28,5 +29,25 @@ class DockerExecutionService
         $prediction = shell_exec($cmd);
 
         return $prediction;
+    }
+
+    public function getModelInfo(UploadedFile $model_file) {
+        $model_filename = $model_file->getClientOriginalName();
+        $temp_dir = sys_get_temp_dir() . '/model_tmp';
+        if (!File::exists($temp_dir)) {
+            File::makeDirectory($temp_dir, 0755, true);
+        }
+
+        $temp_model_path = $temp_dir . '/' . $model_filename;
+
+        File::copy($model_file->getPathname(), $temp_model_path);
+
+        $cmd = "docker run --rm "
+            . "-v {$temp_dir}:/models "
+            . "-e MODEL_PATH=/models/{$model_filename} "
+            . "get_model_info_image python get_model_info.py ";
+        $model_info = json_decode(shell_exec($cmd));
+
+        return $model_info;
     }
 }
