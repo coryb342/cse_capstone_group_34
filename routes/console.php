33a956be6680adf38f78b5b8e35d3f9dc12c9715
Cory Bateman <coryb342@gmail.com>
2026-04-02 18:49:01 -0700

    Modify prediction schedular to look for heartbeat

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Models\SensorViewingSession;
 use App\Models\SoftSensor;
 use App\Services\DockerExecutionService;
 use App\Services\PredictiveModelAnalyticsService;
 use App\Services\SoftSensorService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
@@ -18,7 +18,12 @@ Schedule::call(function () {
     $soft_sensor_service = new SoftSensorService(new DockerExecutionService(), new PredictiveModelAnalyticsService());
     $soft_sensors = SoftSensor::all();
     foreach ($soft_sensors as $soft_sensor) {
-        if ($soft_sensor->last_prediction_time < now()->subSeconds($soft_sensor->time_interval))
-        $soft_sensor_service->updatePredictedValue($soft_sensor);
+        if (SensorViewingSession::query()->where('organization_id', $soft_sensor->organization_id)->where('updated_at', '>', now()->subSeconds(10))->exists()){
+
+            if ($soft_sensor->last_prediction_time < now()->subSeconds($soft_sensor->time_interval)){
+                $soft_sensor_service->updatePredictedValue($soft_sensor);
+
+            }
+        }
     }
 })->everySecond();
